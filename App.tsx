47e86f34cbc0b8a58b7e2113895f2991351e72ec
DiedeKlink <diede@protonmail.com>
2024-08-08 14:23:51 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,35 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+
+import React, { useState } from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
+
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+
+
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -56,63 +43,139 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [randomBackground, setRandomBackground] = useState('#ffffff');
+  const [randomBackgroundSqaure, setrandomBackgroundSqaure] = useState('#000000');
+  const [randomBackgroundCircle, setrandomBackgroundCircle] = useState('#000000');
+  const [randomBackgroundTriangle, setrandomBackgroundTriangle] = useState('#000000');
+  const [randomBackgroundRectangle, setrandomBackgroundRectangle] = useState('#000000');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF'
+
+
+
+    function getColor() {
+      let color = "#"
+      for (let i = 0; i < 6; i++) {
+        color += hexRange[Math.floor(Math.random() * 16)]
+
+      }
+
+      return color
+    }
+    setRandomBackground(getColor())
+    setrandomBackgroundSqaure(getColor())
+    setrandomBackgroundCircle(getColor())
+    setrandomBackgroundTriangle(getColor())
+    setrandomBackgroundRectangle(getColor())
+  }
+
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={"#000000"}></StatusBar>
+      <View style={[styles.container, { backgroundColor: randomBackground }]}>
+        <View style={styles.shapeContainer}>
+          <View style={[styles.shape, styles.square, { backgroundColor: randomBackgroundSqaure }]}>
+            <Text style={styles.shapeText}>{randomBackgroundSqaure}</Text>
+          </View>
+          <View style={[styles.shape, styles.circle, { backgroundColor: randomBackgroundCircle }]}>
+            <Text style={styles.shapeText}>{randomBackgroundCircle}</Text>
+
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn} >
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+
+          </View>
+        </TouchableOpacity>
+        <View style={styles.shapeContainer}>
+          <View style={[styles.shape, styles.triangle, { backgroundColor: randomBackgroundTriangle, borderBottomColor: randomBackgroundTriangle, borderLeftColor: randomBackground, borderTopColor: randomBackground, borderRightColor: randomBackground }]}>
+            <Text style={[styles.shapeText, styles.shapeTextTri]}>{randomBackgroundTriangle}</Text>
+
+
+          </View>
+
+          <View style={[styles.shape, styles.rectangle, { backgroundColor: randomBackgroundRectangle }]}>
+            <Text style={styles.shapeText}>{randomBackgroundRectangle}</Text>
+
+          </View>
+
+        </View>
+
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  sectionTitle: {
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: "#6a1b4d",
+    paddingVertical: 10,
+    paddingHorizontal: 40
+  },
+  actionBtnTxt: {
     fontSize: 24,
-    fontWeight: '600',
+    color: '#ffffff',
+    textTransform: 'uppercase'
+
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  shapeContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '100%'
   },
-  highlight: {
-    fontWeight: '700',
+  shape: {
+    width: '40%',
+    height: '40%',
+
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
   },
+  square: {},
+  circle: {
+    borderRadius: 200
+  },
+  triangle: {
+ 
+    borderLeftWidth: 100,
+
+    borderTopWidth: 100,
+
+    borderRightWidth: 100,
+
+    borderBottomWidth: 126,
+    borderBottomColor: '#000000',
+    width: 0,
+    height: 0,
+    marginTop: -100
+  },
+  rectangle: {
+    height: '30%'
+  },
+  shapeText: {
+    fontSize: 20,
+    color: '#ffffff',
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+  },
+  shapeTextTri: {
+    position: 'absolute',
+    top: 60
+  }
+
 });
 
 export default App;
